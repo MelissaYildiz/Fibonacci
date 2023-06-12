2f381ecc4f7dd31a23d386cbc1a008fadf818306
Meli Yildiz <129263444+MelissaYildiz@users.noreply.github.com>
2023-06-12 10:45:58 +0200

    Add files via upload

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Bitte Gib eine Fibonacci Zahl ein: ");
+
+        // Ich nehme den Input vom Benutzer auf und Speicher sie in einer Variable
+        int grenze = int.Parse(Console.ReadLine());
+
+        // Hier tu ich meine Variable deklarieren und initialisieren also ich setze eine Variable ein und gebe ihr ein wert
+        int vorherigeZahl = 0;
+        int aktuelleZahl = 1; //anfangsbedingung von fibonacci
+        int nächsteZahl = 1;  //anfangsbedingung von fibonacci
+        int counter = 0;
+
+        // Hier setze ich die While Schleife welche aufhört wenn die grenze erreicht ist bei der beliebigen Zahl
+        while (counter < grenze) // das ist die schleife abbruchbedingung 
+        {
+            // In der While Schleife wird die aktuelle Zahl ausgegeben und dann berechnet
+            Console.Write(aktuelleZahl + " ");
+            nächsteZahl = vorherigeZahl + aktuelleZahl; // das ist der erste teil von der Fibonacci Zahl
+            vorherigeZahl = aktuelleZahl;
+            aktuelleZahl = nächsteZahl;
+            counter++;
+        }
+    }
+}
